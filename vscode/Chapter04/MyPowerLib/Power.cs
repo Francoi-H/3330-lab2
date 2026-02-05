9da23f75198202f56a1d536d31dd561c2476de4d
francoih <johndoe@email.com>
2026-02-05 16:39:52 -0500

    Add all Lab 3 library and test projects

---
+++ vscode/Chapter04/MyPowerLib/Power.cs
@@ -0,0 +1,17 @@
+﻿namespace MyPowerLib;
+
+public class Power
+{
+    // Calculates baseNum to the power of exponent
+    public static double MyPow(double baseNum, double exponent)
+    {
+        return Math.Pow(baseNum, exponent);
+    }
+
+    // Calculates (baseNum^exp1)^exp2 [cite: 92]
+    public static double MyPow(double baseNum, double exp1, double exp2)
+    {
+        double firstResult = Math.Pow(baseNum, exp1);
+        return Math.Pow(firstResult, exp2);
+    }
+}
